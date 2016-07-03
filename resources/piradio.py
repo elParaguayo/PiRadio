87e242c6ee0881dc2cabcadb687578df7e5c809c
elParaguayo <antodudgeon@gmail.com>
2016-07-03 08:13:40 +0100

    Fix menu jump

--- resources/piradio.py
+++ resources/piradio.py
@@ -86,10 +86,6 @@ class PiRadio(object):
             # Add the mode-specific menu to our own menu
             self.main_menu.add_item(mode.modemenu)
 
-        # Bind the selection control actions to the main menu
-        self.selector.bind_rotate(self.main_menu.rotate)
-        self.selector.bind_select(self.main_menu.select)
-
         # No mode set at the moment
         self.mode = None
 
@@ -116,6 +112,10 @@ class PiRadio(object):
 
         # Start the time thread
         self._time.start()
+
+        # Bind the selection control actions to the main menu
+        self.selector.bind_rotate(self.main_menu.rotate)
+        self.selector.bind_select(self.main_menu.select)
 
     def exit(self):
         """Method to stop the radio and shutdown gracefully."""
